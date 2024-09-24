b967a92293fb1cf80dd058ce614ccdcc18477c2d
Youssef <abdelazizyoussef2020@gmail.com>
2024-09-24 23:54:54 +0300

    connecting to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
@@ -12,11 +13,16 @@ import 'package:plant_app/Screens/helpers/hiver_helpers.dart';
 import 'package:plant_app/Screens/home/cubit/home_screen_cubit.dart';
 import 'package:plant_app/Screens/profile/cubit/profile_cubit.dart';
 import 'package:plant_app/Screens/splash/splash.dart';
+import 'package:plant_app/firebase_options.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(HiveHelpers.tokenBox);
   await Hive.openBox(HiveHelpers.gardenBox);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  WidgetsFlutterBinding.ensureInitialized();
   DioHelpers.init();
 
   // Set the token before running the app
