cac6d815fab51a32f5afa610c5fe6cc7576a3e88
Walaa-osama <walaaosama209@gmail.com>
2024-09-26 01:09:14 +0300

    Sign_up with google

--- lib/Screens/Login/cubit/login_cubit.dart
+++ lib/Screens/Login/cubit/login_cubit.dart
@@ -1,6 +1,9 @@
 import 'package:bloc/bloc.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:plant_app/Screens/helpers/dio_helpers.dart';
 import 'package:plant_app/Screens/helpers/hiver_helpers.dart';
 import 'package:plant_app/Screens/navigation/navigation_screen.dart';
@@ -12,7 +15,8 @@ part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit() : super(LoginInitial());
-
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
   final key = GlobalKey<FormState>();
@@ -41,6 +45,39 @@ class LoginCubit extends Cubit<LoginState> {
         emit(LoginSucessState());
       } else {
         emit(LogineErrorState(model.message ?? "Error"));
+      }
+    } catch (e) {
+      emit(LogineErrorState(e.toString()));
+    }
+  }
+
+  // Google Sign-In method
+  Future<void> signInWithGoogle() async {
+    emit(LoginLoadingState());
+
+    try {
+      final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+      if (googleUser == null) {
+        emit(LogineErrorState("Sign in aborted by user"));
+        return;
+      }
+
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+
+      final UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
+      final User? user = userCredential.user;
+
+      if (user != null) {
+        emit(LoginSucessState());
+      } else {
+        emit(LogineErrorState("Google sign-in failed"));
       }
     } catch (e) {
       emit(LogineErrorState(e.toString()));
