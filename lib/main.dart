a7caac6ca04386fce828b9d104a4e9f2c708c4cc
Youssef <abdelazizyoussef2020@gmail.com>
2024-09-27 18:43:45 +0300

    connecting to firebase messaging and displaying messages foreground and not active

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
@@ -15,23 +17,75 @@ import 'package:plant_app/Screens/profile/cubit/profile_cubit.dart';
 import 'package:plant_app/Screens/splash/splash.dart';
 import 'package:plant_app/firebase_options.dart';
 
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   await Hive.openBox(HiveHelpers.tokenBox);
   await Hive.openBox(HiveHelpers.gardenBox);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  WidgetsFlutterBinding.ensureInitialized();
-  DioHelpers.init();
 
-  // Set the token before running the app
+  // Request permission for notifications on Android 13+
+  NotificationSettings settings = await FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    print('User granted permission');
+  } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+    print('User granted provisional permission');
+  } else {
+    print('User declined or has not accepted permission');
+  }
+
+  DioHelpers.init();
+  await FirebaseMessaging.instance.subscribeToTopic("plant");
+
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print("fcm Token is $fcmToken");
+
+  // Handle messages when the app is in the foreground
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message while in the foreground!');
+
+    if (message.notification != null) {
+      print('Message also contained a notification title: ${message.notification!.title}');
+      print('Message also contained a notification body: ${message.notification!.body}');
+
+      // Display the AwesomeDialog with the notification details
+      AwesomeDialog(
+        context: navigatorKey.currentContext!, // Use the GlobalKey to get the current context
+        dialogType: DialogType.info,
+        headerAnimationLoop: false,
+        animType: AnimType.bottomSlide,
+        title: message.notification!.title ?? 'Notification',
+        desc: message.notification!.body ?? 'You have received a new message',
+        btnOkOnPress: () {},
+        btnOkText: 'Dismiss',
+        btnOkColor: Colors.blue,
+      ).show();
+    }
+  });
+
+  // Set the background message handler
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
   final token = HiveHelpers.getToken();
   if (token != null && token.isNotEmpty) {
     DioHelpers.setToken(token);
   }
-  // HiveHelpers.clearPlantIds();
+
   runApp(const MainApp());
+}
+
+// Background message handler should be a top-level function
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("Handling a background message: ${message.messageId}");
 }
 
 class MainApp extends StatelessWidget {
@@ -60,8 +114,9 @@ class MainApp extends StatelessWidget {
           create: (context) => SpeciesCubit()..getAllSpecies(),
         ),
       ],
-      child: const GetMaterialApp(
-        home: SplashScreen(),
+      child: GetMaterialApp(
+        navigatorKey: navigatorKey, // Assign the GlobalKey here
+        home: const SplashScreen(),
         debugShowCheckedModeBanner: false,
       ),
     );
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,14 +7,18 @@ import Foundation
 
 import firebase_auth
 import firebase_core
+import firebase_messaging
 import google_sign_in_ios
 import path_provider_foundation
+import rive_common
 import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
