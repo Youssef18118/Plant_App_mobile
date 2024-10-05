ccf7c4536e908c1919750615bece03f7f5d79dec
Walaa-osama <walaaosama209@gmail.com>
2024-10-05 13:12:31 +0300

    Remove Repeated Fun

--- lib/Screens/home/cubit/home_screen_cubit.dart
+++ lib/Screens/home/cubit/home_screen_cubit.dart
@@ -118,17 +118,9 @@ class HomeScreenCubit extends Cubit<HomeScreenState> {
 
   void clearAddedPlants() {
     addedPlantIds.clear();
-    HiveHelpers.clearPlantIds(); 
+    HiveHelpers.clearPlantIds();
 
     // Emit a state update to refresh the UI
-    emit(GettingPlantsSuccess()); 
-  }
-
-  void clearAddedPlants() {
-    addedPlantIds.clear();
-    HiveHelpers.clearPlantIds(); 
-
-    // Emit a state update to refresh the UI
-    emit(GettingPlantsSuccess()); 
+    emit(GettingPlantsSuccess());
   }
 }
